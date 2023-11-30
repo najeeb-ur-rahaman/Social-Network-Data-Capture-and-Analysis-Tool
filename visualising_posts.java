1d9145ad2b518fd6aa145fa8f92cfc749bc6952f
GeorgeWW02 <152439601+GeorgeWW02@users.noreply.github.com>
2023-11-30 19:54:39 +0000

    Update visualising_posts.java

--- visualising_posts.java
+++ visualising_posts.java
@@ -1,5 +1,3 @@
-package visualising_posts;
-
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -8,74 +6,62 @@ import org.jfree.data.category.DefaultCategoryDataset;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class visualising_posts {
-    public static void main(String[] args) {
-        Map<String, Integer> videoViews = readDataFromFile("makeup_data.txt");
+public class PopularPostsChart {
+    public static void createAndShowGUI() {
+        JFrame frame = new JFrame("Popular Posts Chart");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        SwingUtilities.invokeLater(() -> {
-            MakeupVideoChart example = new MakeupVideoChart("Makeup Video Views", videoViews);
-            example.setSize(800, 600);
-            example.setLocationRelativeTo(null);
-            example.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            example.setVisible(true);
-        });
+        // Create chart panel
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
+        chartPanel.setPreferredSize(new Dimension(800, 600));
+        frame.getContentPane().add(chartPanel);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
-
-    private static Map<String, Integer> readDataFromFile(String filePath) {
-        Map<String, Integer> videoViews = new HashMap<>();
-
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(":");
-                if (parts.length == 2) {
-                    String videoName = parts[0].trim();
-                    int views = Integer.parseInt(parts[1].trim());
-                    videoViews.put(videoName, views);
-                }
-            }
-        } catch (IOException | NumberFormatException e) {
-            e.printStackTrace();
-        }
-
-        return videoViews;
+    private static JFreeChart createChart(CategoryDataset dataset) {
+        return ChartFactory.createBarChart(
+                "Most Popular Posts by Likes",
+                "Post ID",
+                "Likes",
+                dataset
+        );
     }
-}
-
-class MakeupVideoChart extends JFrame {
-    public MakeupVideoChart(String title, Map<String, Integer> videoViews) {
-        super(title);
-
-        CategoryDataset dataset = createDataset(videoViews);
-        JFreeChart chart = createChart(dataset);
-
-        ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new Dimension(600, 400));
-        setContentPane(chartPanel);
-    }
-
-    private CategoryDataset createDataset(Map<String, Integer> videoViews) {
+    private static CategoryDataset createDataset() {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 
-        for (Map.Entry<String, Integer> entry : videoViews.entrySet()) {
-            dataset.addValue(entry.getValue(), "Views", entry.getKey());
+        try (Scanner scanner = new Scanner(new File("\"C:\\Users\\gwils\\OneDrive\\Desktop\\video_data.json\""))) {
+            StringBuilder jsonContent = new StringBuilder();
+
+            // Read the entire JSON file into a StringBuilder
+            while (scanner.hasNextLine()) {
+                jsonContent.append(scanner.nextLine());
+            }
+
+            // Assuming the JSON structure includes post data with "postId" and "likes" properties
+            String regex = "\"postId\"\\s*:\\s*\"(.*?)\".*?\"likes\"\\s*:\\s*(\\d+)";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(jsonContent);
+
+            while (matcher.find()) {
+                String postId = matcher.group(1);
+                int likes = Integer.parseInt(matcher.group(2));
+
+                // Add data to the dataset
+                dataset.addValue(likes, "Likes", postId);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
 
         return dataset;
     }
-
-    private JFreeChart createChart(CategoryDataset dataset) {
-        return ChartFactory.createBarChart(
-                "Makeup Video Views",
-                "Video Names",
-                "Views",
-                dataset
-        );
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(PopularPostsChart::createAndShowGUI);
     }
 }
