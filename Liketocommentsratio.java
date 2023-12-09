683379e1f22f25bc66415da446498d64f27843e9
GeorgeWW02 <152439601+GeorgeWW02@users.noreply.github.com>
2023-12-09 19:30:09 +0000

    Update Liketocommentsratio.java

--- Liketocommentsratio.java
+++ Liketocommentsratio.java
@@ -1,82 +1,45 @@
-package sprint2;
+package Final;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.DecimalFormat;
 
 public class Liketocommentsratio {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/testingtestingtesting";
+    private static final String JDBC_USER = "George";
+    private static final String JDBC_PASSWORD = "George02!";
 
     public static void main(String[] args) {
-        // Provide the path to your JSON file on the desktop
-        String filePath = "C:\\Users\\gwils\\OneDrive\\Desktop\\filter data 2.json";
-
         try {
-            calculateAndPrintRatioForEachVideo(filePath);
-        } catch (IOException e) {
+            calculateAndPrintRatioForEachVideo();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    private static void calculateAndPrintRatioForEachVideo(String filePath) throws IOException {
-        // Read JSON file into a string
-        String jsonString = readJsonFile(filePath);
-
-        // Remove leading and trailing whitespaces and newlines for consistent parsing
-        jsonString = jsonString.trim();
-
-        // Extract the "filters" array
-        int filtersIndex = jsonString.indexOf("\"filters\": [");
-        int startIndex = jsonString.indexOf("{", filtersIndex);
-        int endIndex = jsonString.lastIndexOf("}");
-        String filtersArrayString = jsonString.substring(startIndex, endIndex + 1);
-
-        // Split the array into individual video objects
-        String[] videoObjects = filtersArrayString.split("\\},\\s*\\{");
-
-        // Create a DecimalFormat to format the ratio to two significant figures
+    private static void calculateAndPrintRatioForEachVideo() throws SQLException {
         DecimalFormat ratioFormat = new DecimalFormat("#.##");
-
-        // Iterate through each video in the array
-        for (int i = 0; i < videoObjects.length; i++) {
-            String videoObjectString = videoObjects[i];
-
-            // Extract likes and comments counts from the current video
-            int likes = extractFieldValue(videoObjectString, "likeCount");
-            int comments = extractFieldValue(videoObjectString, "commentCount");
-
-            // Get the video title
-            String title = extractFieldValueString(videoObjectString, "title");
-
-            // Calculate the ratio
-            double ratio = (comments != 0) ? (double) likes / comments : Double.POSITIVE_INFINITY;
-
-            // Format the ratio to two significant figures
-            String formattedRatio = ratioFormat.format(ratio);
-
-            // Print the result with the video title and formatted ratio
-            System.out.println("Video: \"" + title + "\" - Likes to Comment Ratio: " + formattedRatio);
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD)) {
+            String query = "SELECT id, title, like_count, comment_count FROM video_data";
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    while (resultSet.next()) {
+                        String videoId = resultSet.getString("id");
+                        String title = resultSet.getString("title");
+                        int likes = resultSet.getInt("like_count");
+                        int comments = resultSet.getInt("comment_count");
+                        double ratio = (comments != 0) ? (double) likes / comments : Double.POSITIVE_INFINITY;
+                        String formattedRatio = ratioFormat.format(ratio);
+                        System.out.println("Video Title: " + title);
+                        System.out.println("Likes to Comment Ratio: " + formattedRatio);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-    }
-
-    private static String readJsonFile(String filePath) throws IOException {
-        Path path = Paths.get(filePath);
-        return Files.readString(path);
-    }
-
-    private static int extractFieldValue(String videoObjectString, String fieldName) {
-        int fieldIndex = videoObjectString.indexOf("\"" + fieldName + "\":");
-        int valueIndex = videoObjectString.indexOf("\"", fieldIndex + fieldName.length() + 3);
-        int endIndex = videoObjectString.indexOf("\"", valueIndex + 1);
-        String valueString = videoObjectString.substring(valueIndex + 1, endIndex);
-        return Integer.parseInt(valueString);
-    }
-
-    private static String extractFieldValueString(String videoObjectString, String fieldName) {
-        int fieldIndex = videoObjectString.indexOf("\"" + fieldName + "\":");
-        int valueIndex = videoObjectString.indexOf("\"", fieldIndex + fieldName.length() + 3);
-        int endIndex = videoObjectString.indexOf("\"", valueIndex + 1);
-        return videoObjectString.substring(valueIndex + 1, endIndex);
     }
 }
