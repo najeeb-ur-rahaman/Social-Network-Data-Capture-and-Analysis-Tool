92cb2729aa1f254293d66c9011849c062da6b43b
GeorgeWW02 <152439601+GeorgeWW02@users.noreply.github.com>
2023-12-09 19:39:40 +0000

    Update TagsReader.java

--- TagsReader.java
+++ TagsReader.java
@@ -1,48 +1,46 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+package Final;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class TagsReader {
-
-    public static Map<String, Integer> tallyTagsFromFile(String filePath) {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/testingtestingtesting";
+    private static final String JDBC_USER = "George";
+    private static final String JDBC_PASSWORD = "George02!";
+    public static Map<String, Integer> tallyTagsFromDatabase() {
         Map<String, Integer> tagCounts = new LinkedHashMap<>();
 
-        try (Scanner scanner = new Scanner(new File(filePath))) {
-            StringBuilder jsonContent = new StringBuilder();
-            while (scanner.hasNextLine()) {
-                jsonContent.append(scanner.nextLine());
-            }
-            Pattern pattern = Pattern.compile("\"tags\"\\s*:\\s*\\[([^\\]]*)\\]");
-            Matcher matcher = pattern.matcher(jsonContent);
-
-            if (matcher.find()) {
-                String tagsArray = matcher.group(1);
-
-                String[] tagTokens = tagsArray.split(",");
-                for (String token : tagTokens) {
-                    String tag = token.trim().replaceAll("\"", "");
-                    tagCounts.put(tag, tagCounts.getOrDefault(tag, 0) + 1);
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD)) {
+            String query = "SELECT tags FROM video_data";
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    while (resultSet.next()) {
+                        String tags = resultSet.getString("tags");
+                        if (tags != null) {
+                            String[] tagTokens = tags.split(",");
+                            for (String token : tagTokens) {
+                                String tag = token.trim().replaceAll("\"", "");
+                                tagCounts.put(tag, tagCounts.getOrDefault(tag, 0) + 1);
+                            }
+                        }
+                    }
                 }
-
-                System.out.println("Tag Counts: " + tagCounts);
-            } else {
-                System.out.println("No 'tags' array found in the JSON file.");
             }
-        } catch (FileNotFoundException e) {
-            System.err.println("Error reading JSON file: " + e.getMessage());
+
+            System.out.println("Tag Counts: " + tagCounts);
+        } catch (SQLException e) {
+            System.err.println("Error accessing the database: " + e.getMessage());
         }
 
         return tagCounts;
     }
 
     public static void main(String[] args) {
-        String jsonFilePath = "C:\\Users\\gwils\\OneDrive\\Desktop\\video_data.json";
-        Map<String, Integer> tagCounts = tallyTagsFromFile(jsonFilePath);
+        Map<String, Integer> tagCounts = tallyTagsFromDatabase();
     }
 }
