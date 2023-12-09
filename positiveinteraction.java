8a42a32402e940580a3e22987465d4c5c70835e6
GeorgeWW02 <152439601+GeorgeWW02@users.noreply.github.com>
2023-12-09 19:53:12 +0000

    Update positiveinteraction.java

--- positiveinteraction.java
+++ positiveinteraction.java
@@ -1,88 +1,94 @@
-package sprint2;
+package Final;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.axis.CategoryAxis;
 import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 import javax.swing.*;
 import java.awt.*;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+public class positiveinteraction {
 
-public class positiveinteraction extends JFrame {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/testingtestingtesting";
+    private static final String JDBC_USER = "George";
+    private static final String JDBC_PASSWORD = "George02!";
 
-    public positiveinteraction(String title) {
-        super(title);
-        XYSeries shortSeries = readDataFromJSON("C:\\Users\\gwils\\OneDrive\\Desktop\\filter data 2.json", 5);
-        XYSeries longSeries = readDataFromJSON("C:\\Users\\gwils\\OneDrive\\Desktop\\filter data 2.json", Double.MAX_VALUE);
-        XYSeriesCollection dataset = new XYSeriesCollection();
-        dataset.addSeries(shortSeries);
-        dataset.addSeries(longSeries);
-        JFreeChart chart = ChartFactory.createTimeSeriesChart(
-                "Positive Interaction Trend for Short and Long Posts",
-                "Date",
-                "Number of Likes",  // Update y-axis label
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> createAndShowGui());
+    }
+
+    private static void createAndShowGui() {
+        JFrame frame = new JFrame("Video with Most Likes");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        CategoryDataset dataset = createDataset();
+        JFreeChart chart = createChart(dataset);
+
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new Dimension(600, 400));
+        frame.setContentPane(chartPanel);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private static CategoryDataset createDataset() {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD)) {
+            String query = "SELECT title, like_count FROM video_data ORDER BY like_count DESC LIMIT 5";
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    while (resultSet.next()) {
+                        String title = resultSet.getString("title");
+                        int likeCount = resultSet.getInt("like_count");
+
+                        dataset.addValue(likeCount, "Likes", title);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return dataset;
+    }
+
+    private static JFreeChart createChart(CategoryDataset dataset) {
+        JFreeChart chart = ChartFactory.createBarChart(
+                "Top 5 Videos with Most Likes",
+                "Videos",
+                "Likes",
                 dataset,
-                false,
+                PlotOrientation.VERTICAL,
+                true,
                 true,
                 false
         );
-        XYPlot plot = (XYPlot) chart.getPlot();
-        DateAxis dateAxis = (DateAxis) plot.getDomainAxis();
-        dateAxis.setDateFormatOverride(new SimpleDateFormat("yyyy-MM-dd"));
-        ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new Dimension(800, 600));
-        setContentPane(chartPanel);
-    }
 
-    private XYSeries readDataFromJSON(String filePath, double durationThreshold) {
-        XYSeries series = new XYSeries(durationThreshold == Double.MAX_VALUE ? "Long Posts" : "Short Posts");
-        JSONParser parser = new JSONParser();
-        try {
-            Object obj = parser.parse(new FileReader(filePath));
-            JSONObject jsonObject = (JSONObject) obj;
-            JSONArray filters = (JSONArray) jsonObject.get("filters");
-            for (Object filterObj : filters) {
-                JSONObject filter = (JSONObject) filterObj;
-                String dateString = (String) filter.get("publishedAt");
-                Date date = new SimpleDateFormat("yyyy-MM-dd").parse(dateString);
-                long numberOfLikes = (Long) filter.get("likeCount");  // Update to likeCount
-                String durationString = (String) filter.get("duration");
-                double duration = getDurationInSeconds(durationString);
-                if ((durationThreshold == Double.MAX_VALUE && duration > durationThreshold) ||
-                        (durationThreshold != Double.MAX_VALUE && duration <= durationThreshold)) {
-                    series.add(date.getTime(), numberOfLikes);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return series;
-    }
+        CategoryPlot plot = (CategoryPlot) chart.getPlot();
+        CategoryAxis domainAxis = plot.getDomainAxis();
+        domainAxis.setCategoryMargin(0.25);
 
-    private double getDurationInSeconds(String durationString) {
-        String[] tokens = durationString.split("PT");
-        int minutes = tokens[1].contains("M") ? Integer.parseInt(tokens[1].split("M")[0]) : 0;
-        int seconds = tokens[1].contains("S") ? Integer.parseInt(tokens[1].split("S")[0]) : 0;
-        return minutes * 60 + seconds;
-    }
+        NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            positiveinteraction example = new positiveinteraction("Positive Interaction Trend for Short and Long Posts");
-            example.setSize(800, 600);
-            example.setLocationRelativeTo(null);
-            example.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            example.setVisible(true);
-        });
+        BarRenderer renderer = (BarRenderer) plot.getRenderer();
+        renderer.setItemMargin(0.05);
+
+        return chart;
     }
 }
