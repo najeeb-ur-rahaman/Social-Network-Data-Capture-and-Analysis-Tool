7ffc57b37e28013261728d0a99b075b6cd0f2637
GeorgeWW02 <152439601+GeorgeWW02@users.noreply.github.com>
2023-12-10 18:51:18 +0000

    Update visualising_posts.java

--- visualising_posts.java
+++ visualising_posts.java
@@ -11,7 +11,6 @@ import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -19,7 +18,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class visualising_posts extends ApplicationFrame {
-
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/testingtestingtesting";
     private static final String JDBC_USER = "George";
     private static final String JDBC_PASSWORD = "George02!";
@@ -32,38 +30,28 @@ public class visualising_posts extends ApplicationFrame {
         chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
         setContentPane(chartPanel);
     }
-
-    private XYSeriesCollection createDataset() {
+    public static XYSeriesCollection createDataset() {
         XYSeriesCollection dataset = new XYSeriesCollection();
-
         try (Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD)) {
             String query = "SELECT duration, view_count, title FROM video_data";
             try (PreparedStatement statement = connection.prepareStatement(query)) {
                 try (ResultSet resultSet = statement.executeQuery()) {
                     XYSeries series = new XYSeries("Views");
-
                     while (resultSet.next()) {
                         int videoLength = resultSet.getInt("duration");
                         int totalViews = resultSet.getInt("view_count");
                         String videoTitle = resultSet.getString("title");
-
-                        // Print statements for debugging
-                        System.out.println("Duration: " + videoLength + ", Views: " + totalViews + ", Title: " + videoTitle);
-
                         series.add(videoLength, totalViews);
                         series.setKey(videoTitle);
                     }
-
                     dataset.addSeries(series);
                 }
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return dataset;
     }
-
     private JFreeChart createChart(XYSeriesCollection dataset) {
         JFreeChart chart = ChartFactory.createScatterPlot(
                 "Views vs. Duration",
@@ -71,21 +59,39 @@ public class visualising_posts extends ApplicationFrame {
                 "Views",
                 dataset
         );
-
         XYPlot plot = (XYPlot) chart.getPlot();
         NumberAxis domainAxis = (NumberAxis) plot.getDomainAxis();
         domainAxis.setTickUnit(new NumberTickUnit(1.0)); // Adjust the tick unit as needed
-
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
         plot.setRenderer(renderer);
-
         return chart;
     }
-
     public static void main(String[] args) {
         visualising_posts chart = new visualising_posts("Views vs. Duration");
         chart.pack();
         RefineryUtilities.centerFrameOnScreen(chart);
         chart.setVisible(true);
+    } 
+    public static XYSeriesCollection createMutatedDatasetforvisaluasing() {
+        XYSeriesCollection mutatedDataset = new XYSeriesCollection();
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD)) {
+            String query = "SELECT duration, view_count, title FROM video_data";
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    XYSeries mutatedSeries = new XYSeries("Views");
+                    while (resultSet.next()) {
+                        int videoLength = resultSet.getInt("duration");
+                        int totalViews = resultSet.getInt("view_count");
+                        // Introduce mutation by setting a constant key for each iteration
+                        mutatedSeries.add(videoLength, totalViews);
+                        mutatedSeries.setKey("MutatedTitle");
+                    }
+                    mutatedDataset.addSeries(mutatedSeries);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return mutatedDataset;
     }
 }
